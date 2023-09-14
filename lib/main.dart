9da60db6673fa2fc3aaffb3621cb24b3ba2bc86b
kjh2s <kjh2305a@gmail.com>
2023-09-14 17:15:01 +0900

    230914 UI mini 테스트(한글 출력 확인)
    - 리눅스에서 한글 나오는지 빌드 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({Key? key}): super(key: key){
+    print("MyApp() constructor");
+  }
 
   @override
   Widget build(BuildContext context) {
-    /*return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'title',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );*/
-    return CupertinoApp(
-      home: const MyHomePage(title: 'Flutter Demo'),
+      home: const MyHomePage(title: '타이틀 한글 테스트 상태 전달...'),
     );
   }
 }
@@ -42,69 +41,82 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _decrementCounter() {
+    setState(() {
+      if(_counter>0)
+        _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    /*return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        leading: Icon(Icons.account_balance),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'st: You have pushed the button this many times:',
-              // style: GoogleFonts.aladin(
-              //   fontSize: 30,
-              //   fontWeight: FontWeight.w700,
-              //   fontStyle: FontStyle.normal,
-              // ),
+              '텍스트 한글 테스트:',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );*/
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        middle: Text(widget.title),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'st: You have pushed the button this many times:',
-              // style: GoogleFonts.aladin(
-              //   fontSize: 30,
-              //   fontWeight: FontWeight.w700,
-              //   fontStyle: FontStyle.normal,
-              // ),
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+                'aaa'
             ),
-            Center(
-              child: CupertinoButton.filled(
-                onPressed: _incrementCounter,
-                child: const Icon(Icons.add),
+            Container(
+              margin: EdgeInsets.all(10),
+              width: 200,
+              height: 100,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.lightGreenAccent,
+                  width: 10,
+                  style: BorderStyle.solid,
+                  strokeAlign: BorderSide.strokeAlignInside,
+                ),
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30),
+                  topLeft: Radius.circular(30),
+                  bottomLeft: Radius.circular(30),
+                ),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Colors.purple, Colors.white],
+                ),
+                boxShadow: [
+                  BoxShadow(color: Colors.grey,
+                  spreadRadius: 4.0,
+                  blurRadius: 5.0,
+                  blurStyle: BlurStyle.normal),
+                ]
               ),
             ),
           ],
         ),
       ),
-
+      floatingActionButton: Row(
+        children: [
+          SizedBox(width: 30),
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      )
     );
   }
 }
