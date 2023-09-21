9c42fbeec6d8405426b3952f89dcbdf854125f4b
kjh2s <kjh2305a@gmail.com>
2023-09-21 17:46:11 +0900

    230921 DB 기능 테스트1
    - 윈도우에서 sqflite_common_ffi 라이브러리 사용. Create, Read 기능 확인. 리눅스에서 가능 여부는 아직 모름.

---
+++ lib/db_helper.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:path/path.dart';
+import 'model_stock.dart';
+
+final String TableName = 'Stocks';
+
+class DBHelper{
+  static final DBHelper _instance = DBHelper._internal();
+  factory DBHelper(){
+    return _instance;
+  }
+  DBHelper._internal();
+
+  static Database? _database;
+  Future<Database> get database async{
+    if(_database != null) return _database!;
+
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async{
+    sqfliteFfiInit();
+    var db = await databaseFactoryFfi.openDatabase(inMemoryDatabasePath);
+    await db.execute(
+      "Create Table $TableName(stock_ticker TEXT PRIMARY Key, stock_price REAL)",
+    );
+    return db;
+
+    /*Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final database = createDatabaseFactoryFfi(join(documentsDirectory.path, 'stocks.db'),
+        onCreate: (db, version) async{
+      await db.execute(
+        "Create Table $TableName(stock_ticker TEXT PRIMARY Key, stock_price REAL)",
+      );
+    }, version: 1);
+    return database;*/
+  }
+
+  Future<void> insertStock(Stock stock) async{
+    final db = await database;
+
+    await db.insert(TableName,
+        {'stock_ticker': stock.ticker,
+      'stock_price':stock.price}, conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<List<Stock>> all_stocks() async{
+    final db = await database;
+
+    final List<Map<String, dynamic>> maps = await db.query('Stocks');
+
+    return List.generate(maps.length, (i){
+      return Stock(maps[i]['stock_ticker'],
+          maps[i]['stock_price']);
+    });
+  }
+}
+
---
+++ lib/model_stock.dart
@@ -0,0 +1,6 @@
+class Stock{
+  final String ticker;
+  final double price;
+
+  Stock(this.ticker, this.price);
+}
